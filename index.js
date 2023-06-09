5655056acfcb7f7f652795622ad06097bd9a6fa8
Lucas do Couto <48269935+LucasDoCouto@users.noreply.github.com>
2023-06-09 18:08:47 -0400

    JavaScript file

---
+++ index.js
@@ -0,0 +1,8 @@
+const sendButton = document.querySelector(".btn-send")
+
+sendButton.addEventListener("click",function(){
+//   const isExpanded = JSON.parse(menuButton.getAttribute("aria-expanded"));
+//   console.log(isExpanded);
+//   menuButton.setAttribute("aria-expanded", !isExpanded)
+    alert("teste")
+})
