1e157428448ada4ba4ef288d5897e213190405a1
Lucas do Couto <48269935+LucasDoCouto@users.noreply.github.com>
2023-06-10 09:08:32 -0400

    Pointer cursor

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const sendButton = document.querySelector(".btn-send")
 
-sendButton.addEventListener("click",function(){
-//   const isExpanded = JSON.parse(menuButton.getAttribute("aria-expanded"));
-//   console.log(isExpanded);
-//   menuButton.setAttribute("aria-expanded", !isExpanded)
-    alert("teste")
-})
+// sendButton.addEventListener("click",function(){
+// //   const isExpanded = JSON.parse(menuButton.getAttribute("aria-expanded"));
+// //   console.log(isExpanded);
+// //   menuButton.setAttribute("aria-expanded", !isExpanded)
+//     alert("teste")
+// })
+
+
+function validacaoEmail(field) {
+    usuario = field.value.substring(0, field.value.indexOf("@"));
+    dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
+    
+    if ((usuario.length >=1) &&
+        (dominio.length >=3) &&
+        (usuario.search("@")==-1) &&
+        (dominio.search("@")==-1) &&
+        (usuario.search(" ")==-1) &&
+        (dominio.search(" ")==-1) &&
+        (dominio.search(".")!=-1) &&
+        (dominio.indexOf(".") >=1)&&
+        (dominio.lastIndexOf(".") < dominio.length - 1)) {
+    document.getElementById("msgemail").innerHTML="E-mail válido";
+    alert("E-mail valido");
+    }
+    else{
+    document.getElementById("msgemail").innerHTML="<font color='red'>E-mail inválido </font>";
+    alert("E-mail invalido");
+    }
+}
